0dd7dbaf253bc3c88cfbfd49279bfde50398c291
Hieu Hoang (Calvin) <hiehoang@ucdavis.edu>
2025-04-10 22:42:09 -0700

    Initial commit to calvin_branch

---
+++ Mood-App/Mood_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_AppApp.swift
+//  Mood-App
+//
+//  Created by Hieu Hoang on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Mood_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mood-AppTests/Mood_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_AppTests.swift
+//  Mood-AppTests
+//
+//  Created by Hieu Hoang on 4/10/25.
+//
+
+import Testing
+@testable import Mood_App
+
+struct Mood_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
