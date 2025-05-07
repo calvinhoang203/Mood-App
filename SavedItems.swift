4024755aeb704c4f3cdb5136b98598ee6e6c5aa2
ashtheflash1212 <168306086+ashtheflash1212@users.noreply.github.com>
2025-05-06 18:10:42 -0700

    Update SavedItems.swift

--- SavedItems.swift
+++ SavedItems.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 class SavedItems: ObservableObject {
+    
+    /* @Published --> property wrapper: Notifying other views of Update and views being updated with the change
+     didset --> property observer: when activity added or removed, didset calls saveToUserDefaults */
+    
     @Published var savedActivities: [Activity] {
         didSet { saveToUserDefaults(savedActivities, key: "savedActivities") }
     }
@@ -24,6 +28,13 @@ class SavedItems: ObservableObject {
         self.addedResources = Self.loadFromUserDefaults(key: "addedResources") ?? []
     }
 
+
+    /*These two functions use UserDefaults to save and load data
+      Saves Data even when iPhone turns off or restarts
+      Codable - encoding Data and Encoding
+      JSONEncoder = Swift objects to JSON data
+      JSONDecoder = JSON data back to Swift Objects */
+    
     private func saveToUserDefaults<T: Codable>(_ value: T, key: String) {
         if let encoded = try? JSONEncoder().encode(value) {
             UserDefaults.standard.set(encoded, forKey: key)
@@ -38,6 +49,10 @@ class SavedItems: ObservableObject {
         return decoded
     }
 
+
+    /*let index = [code] tells swiftui to find the first instance of activity being indexed and then remove it at that index
+                   if activity exists, remove, if it doesn't, add */
+    
     func toggleActivity(_ activity: Activity) {
         if let index = savedActivities.firstIndex(of: activity) {
             savedActivities.remove(at: index)
@@ -53,7 +68,7 @@ class SavedItems: ObservableObject {
             savedResources.append(resource)
         }
     }
-
+    //Straightforward, if activity saved, return True
     func isActivitySaved(_ activity: Activity) -> Bool {
         savedActivities.contains(activity)
     }
@@ -62,19 +77,27 @@ class SavedItems: ObservableObject {
         savedResources.contains(resource)
     }
 
+    // MARK: - Toggle Planner Adds
 
-    func addActivityToPlanner(_ activity: Activity) {
-        if !addedActivities.contains(activity) {
+    func toggleAddedActivity(_ activity: Activity) {
+        if let index = addedActivities.firstIndex(of: activity) {
+            addedActivities.remove(at: index)
+        } else {
             addedActivities.append(activity)
         }
     }
-
+    /*Same logic for the third row in ResourcePersonalView...checking if the activity is ADDED now and not just saved
+      Used for when switching between filled heart for saved or blank heart if not,
+      purple to green when activity added... etc */
+    
     func isActivityAdded(_ activity: Activity) -> Bool {
         addedActivities.contains(activity)
     }
 
-    func addResourceToPlanner(_ resource: Resource) {
-        if !addedResources.contains(resource) {
+    func toggleAddedResource(_ resource: Resource) {
+        if let index = addedResources.firstIndex(of: resource) {
+            addedResources.remove(at: index)
+        } else {
             addedResources.append(resource)
         }
     }
