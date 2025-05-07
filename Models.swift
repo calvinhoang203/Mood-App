460f410011c248f03974be912180523c77d7a40c
ashtheflash1212 <168306086+ashtheflash1212@users.noreply.github.com>
2025-05-06 18:09:42 -0700

    Update Models.swift

--- Models.swift
+++ Models.swift
@@ -1,6 +1,10 @@
 import Foundation
-import SwiftUI
 
+/* Identifiable allows Activity and Resource objects to be iterated through ForEach loops
+   Codable: Encoding and Decoding of Swift Objects <--> JSON
+            JSON file format allows Swift to save data into a storable format
+   Hashbale: Unique hash values for each Activity and Resource Object
+             Allows Swift to use the .contains() and .firstIndexOf() functions */
 struct Activity: Identifiable, Codable, Hashable {
     var id: String { name } // makes id deterministic
     let name: String
